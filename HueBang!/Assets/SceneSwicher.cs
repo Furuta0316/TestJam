ba414478017b2d61be8d5f7a00a59a27bbe58ba3
fujikuraiori <s1323060@cce.kanagawa-it.ac.jp>
2016-01-28 18:04:58 +0900

    TitleCreate

---
+++ HueBang!/Assets/SceneSwicher.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class SceneSwicher : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnGUI()
+    {
+
+        Event e = Event.current;
+        if (e.button == 0 && e.isMouse)
+        {
+            Debug.Log("111");
+            SceneManager.LoadScene("Ending");
+        }
+    }
+}
